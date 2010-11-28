f511f54b4bd60ad07d3692d7c00288fd7ee7ad33
Steve Lacey <steve@stevelacey.net>
2010-11-28 23:54:56 +0000

    Swapped attending and speaking around on user profile.

--- apps/frontend/modules/user/templates/showSuccess.php
+++ apps/frontend/modules/user/templates/showSuccess.php
@@ -1,14 +1,14 @@
 <h1><?php echo $user->getName() ?></h1>
 <img src="<?php echo $user->getImage() ?>"/>
 
-<?php if($user->getAttending()->count()) : ?>
-  <h2>Attending</h2>
-  <?php include_partial('event/short_list', array('events' => $user->getAttending())) ?>
-<?php endif ?>
-
 <?php if($user->getSpeaking()->count()) : ?>
   <h2>Speaking</h2>
   <?php include_partial('event/short_list', array('events' => $user->getSpeaking())) ?>
+<?php endif ?>
+
+<?php if($user->getAttending()->count()) : ?>
+  <h2>Attending</h2>
+  <?php include_partial('event/short_list', array('events' => $user->getAttending())) ?>
 <?php endif ?>
 
 <?php if($user->getWatching()->count()) : ?>
