c220eb176204698e4f7228a9374b9b4f5264611f
Steve Lacey <steve@stevelacey.net>
2010-12-02 00:34:01 +0000

    Threw in functionality to add new users on the fly, make this dry later- takes bits that should be properly modularised in sfTwitterAuthPlugin.

--- apps/frontend/lib/myEventActions.class.php
+++ apps/frontend/lib/myEventActions.class.php
@@ -32,8 +32,34 @@ class myEventActions extends sfActions {
   public function executeAddSpeaker(sfWebRequest $request) {
     $user = Doctrine::getTable('sfGuardUser')->findOneByUsername($request->getParameter('username'));
 
+    if(!$user instanceOf sfGuardUser) {
+      $xml = Twitter::getConnection()->get('users/show', array('screen_name' => $request->getParameter('username')));
+      
+      if(isset($xml->id)) {
+        $user = new sfGuardUser();
+        $user->setId((int) $xml->id);
+        $user->setUsername($xml->screen_name);
+        $user->setEmailAddress($xml->screen_name);
+        $user->setFirstName(substr($xml->name, 0, strpos($xml->name, ' ')));
+        $user->setLastName(substr($xml->name, strpos($xml->name, ' ') + 1, strlen($xml->name)));
+        $user->setPassword($this->generatePassword());
+
+        $profile = new sfGuardUserProfile();
+        $profile->setId((int) $xml->id);
+        $profile->setName($xml->name);
+        $profile->setDescription($xml->description);
+        $profile->setWebsite($xml->url);
+        $profile->setImage($xml->profile_image_url);
+
+        $user->setProfile($profile);
+        $user->save();
+      }
+    }
+
     if($user instanceOf sfGuardUser) {
       $this->go($request, $user, 'speak', true);
+    } else {
+      $this->redirect('speakers', $this->getRoute()->getObject());
     }
   }
 
@@ -49,5 +75,17 @@ class myEventActions extends sfActions {
 //    $request->checkCSRFProtection();
     call_user_func_array(array($user, $relation), array($this->getRoute()->getObject(), $action));
     $this->redirect($this->getUser()->getReferer($request->getReferer()));
+  }
+
+  private function generatePassword() {
+    // Set a secure, random sfGuard password to ensure that this
+    // account is not wide open if conventional logins are permitted
+    $guid = '';
+
+    for ($i = 0; ($i < 8); $i++) {
+      $guid .= sprintf("%x", mt_rand(0, 15));
+    }
+
+    return $guid;
   }
 }
