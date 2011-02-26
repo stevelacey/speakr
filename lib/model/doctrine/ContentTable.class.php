637a4941e0f20962e627494c6b3eaed112632a5f
Steve Lacey <steve@stevelacey.net>
2011-02-26 23:44:43 +0000

    Cut out content speakr slug dependence.

--- lib/model/doctrine/ContentTable.class.php
+++ lib/model/doctrine/ContentTable.class.php
@@ -4,16 +4,4 @@ class ContentTable extends Doctrine_Table {
   public static function getInstance() {
     return Doctrine_Core::getTable('Content');
   }
-
-  public function findBySpeakerSlugAndSlug($params) {
-    return Doctrine_Query::create()->
-      select('*, GROUP_CONCAT(DISTINCT(u.username) ORDER BY u.username ASC SEPARATOR "-and-") as ss')->
-      from('Content c')->
-      leftJoin('c.Presentations p')->
-      leftJoin('p.PresentationUser pu')->
-      leftJoin('pu.User u')->
-      having('ss = ?', $params['speaker_slug'])->
-      andWhere('c.slug = ?', $params['slug'])->
-      fetchOne();
-  }
 }
