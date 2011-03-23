bcb814e5fb3f581a4e36a44d687e71b79fd3dc03
Steve Lacey <steve@stevelacey.net>
2011-03-23 23:02:18 +0000

    Added helper message for empty schedule pages.

--- apps/frontend/modules/schedule/templates/friendsSuccess.php
+++ apps/frontend/modules/schedule/templates/friendsSuccess.php
@@ -2,6 +2,10 @@
   <?php include_partial('nav') ?>
   <div class="events">
     <h2>Friends Events</h2>
-    <?php include_partial('event/short_list', array('events' => $events)) ?>
+    <?php if($events->count()) : ?>
+      <?php include_partial('event/short_list', array('events' => $events)) ?>
+    <?php else : ?>
+      <p>It doesn't look like anyone you follow is attending any events on Speakr, why not check out <?php echo link_to('upcoming events', 'upcoming_events') ?>?</p>
+    <?php endif ?>
   </div>
 </div>
--- apps/frontend/modules/schedule/templates/watchingSuccess.php
+++ apps/frontend/modules/schedule/templates/watchingSuccess.php
@@ -2,6 +2,10 @@
   <?php include_partial('nav') ?>
   <div class="events">
     <h2>Your Watching</h2>
-    <?php include_partial('event/short_list', array('events' => $events)) ?>
+    <?php if($events->count()) : ?>
+      <?php include_partial('event/short_list', array('events' => $events)) ?>
+    <?php else : ?>
+      <p>It doesn't look like you're watching any events, why not check out <?php echo link_to('upcoming events', 'upcoming_events') ?>?</p>
+    <?php endif ?>
   </div>
 </div>
